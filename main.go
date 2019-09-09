4accfa114f40d5cc15b69984767822cd818b9808
Roni Yusuf Manalu <rymanalu@gmail.com>
2019-09-09 20:51:44 +0700

    002

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Hello World!"})
 	})
+
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		c.JSON(200, gin.H{"message": fmt.Sprintf("Hello, %s!", name)})
+	})
+
 	r.Run()
 }
